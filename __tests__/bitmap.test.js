697319ace2f22bed083d0215ac78dca2b908349d
Austin Jess <ajess33@gmail.com>
2019-05-25 22:27:18 -0500

    completed test for parsing a file

--- __tests__/bitmap.test.js
+++ __tests__/bitmap.test.js
@@ -11,7 +11,23 @@ const bmpPath = `${__dirname}/../assets/baldy.bmp`;
 describe('The Bitmap Module', () => {
   it('can parse a file', async () => {
     const buffer = await fsReadFile(bmpPath);
-
     expect(buffer).toBeDefined();
+
+    let bitmap = new Bitmap(bmpPath);
+    bitmap.parse(buffer);
+
+    expect(bitmap.buffer).toBe(buffer);
+    expect(bitmap.type).toBe('BM');
+
+    expect(bitmap.size).toBe(15146);
+    expect(bitmap.offset).toBe(1146);
+
+    expect(bitmap.width).toBe(110);
+    expect(bitmap.height).toBe(125);
+
+    expect(bitmap.bitsPerPixel).toBe(8);
+
+    expect(bitmap.colorBuffer).toBeDefined();
+    expect(bitmap.pixelBuffer).toBeDefined();
   });
 });
